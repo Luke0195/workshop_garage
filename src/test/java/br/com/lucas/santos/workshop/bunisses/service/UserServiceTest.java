cc5de3020e052c68a0e8789904c8569d4adbce3c
Lucas Santos <lucas201273@gmail.com>
2025-05-01 17:33:53 -0300

    test: ensure UserService.add should throws a ServerError if encrypter throws

--- src/test/java/br/com/lucas/santos/workshop/bunisses/service/UserServiceTest.java
+++ src/test/java/br/com/lucas/santos/workshop/bunisses/service/UserServiceTest.java
@@ -5,6 +5,7 @@ import br.com.lucas.santos.workshop.bunisses.protocols.Encrypter;
 import br.com.lucas.santos.workshop.dto.request.UserRequestDto;
 import br.com.lucas.santos.workshop.factories.UserFactory;
 import br.com.lucas.santos.workshop.infrastructure.exceptions.ResourceAlreadyExistsException;
+import br.com.lucas.santos.workshop.infrastructure.exceptions.ServerError;
 import br.com.lucas.santos.workshop.infrastructure.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -56,6 +57,15 @@ class UserServiceTest {
     void addShouldEncrypterWithCorrectValue(){
         userService.add(userRequestDto);
         Mockito.verify(encrypter).encrypt(userRequestDto.password());
+    }
+
+    @DisplayName("add should throws ServerError if encrypter throws")
+    @Test
+    void addShouldThrowsServerErrorIfEncrypterThrows(){
+        Mockito.when(encrypter.encrypt(Mockito.anyString())).thenReturn(null);
+        Assertions.assertThrows(ServerError.class, () -> {
+            userService.add(userRequestDto);
+        });
     }
 
 }
