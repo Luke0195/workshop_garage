f4e6628d1c6dbc7ea8cbb8aa8db78347aa7773c2
Lucas Santos <lucas201273@gmail.com>
2025-05-02 14:52:38 -0300

    test: ensure AuthenticationService.authenticate calls UserRepositoryFindByEmail when valid e-mail is provided

--- src/test/java/br/com/lucas/santos/workshop/bunisses/service/AuthenticationServiceTest.java
+++ src/test/java/br/com/lucas/santos/workshop/bunisses/service/AuthenticationServiceTest.java
@@ -1,12 +1,15 @@
 package br.com.lucas.santos.workshop.bunisses.service;
 
 import br.com.lucas.santos.workshop.domain.dto.request.AuthenticationRequestDto;
+import br.com.lucas.santos.workshop.factories.AuthenticationFactory;
 import br.com.lucas.santos.workshop.infrastructure.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -21,18 +24,26 @@ class AuthenticationServiceTest {
     private UserRepository userRepository;
 
     @InjectMocks
-    private AuthenticationService authenticationService;
+    private AuthenticationService sut;
 
 
-    @DisplayName("authenticatie should calls findByEmail when valid e-mail is provided")
+    @BeforeEach
+    void setup(){
+        setupValues();
+    }
+
+
+
+    @DisplayName("authenticate should calls UserRepository findByEmail when valid e-mail is provided")
     @Test
     void authenticateShouldCallFindByEmailWhenValidEmailIsProvided(){
-
+        sut.authenticate(this.authenticationRequestDto);
+        Mockito.verify(userRepository).findByEmail(this.authenticationRequestDto.email());
     }
 
 
     void setupValues(){
-        this.authenticationRequestDto =
+        this.authenticationRequestDto = AuthenticationFactory.makeAuthenticationRequestDto();
     }
 
 
