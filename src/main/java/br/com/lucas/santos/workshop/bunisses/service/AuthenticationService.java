687ba03666f38a8a257435fba9a2554cfd3e13e4
Lucas Santos <lucas201273@gmail.com>
2025-05-02 14:52:31 -0300

    feat: ensure AuthenticationService.authenticate calls UserRepositoryFindByEmail when valid e-mail is provided

--- src/main/java/br/com/lucas/santos/workshop/bunisses/service/AuthenticationService.java
+++ src/main/java/br/com/lucas/santos/workshop/bunisses/service/AuthenticationService.java
@@ -3,6 +3,7 @@ package br.com.lucas.santos.workshop.bunisses.service;
 import br.com.lucas.santos.workshop.domain.dto.request.AuthenticationRequestDto;
 import br.com.lucas.santos.workshop.domain.dto.response.AuthenticationResponseDto;
 import br.com.lucas.santos.workshop.domain.usecases.authentication.Authentication;
+import br.com.lucas.santos.workshop.infrastructure.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,10 +11,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthenticationService implements Authentication {
 
 
+    private final UserRepository userRepository;
+
+    public AuthenticationService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
     @Override
     @Transactional
     public AuthenticationResponseDto authenticate(AuthenticationRequestDto authenticationRequestDto) {
+        userRepository.findByEmail(authenticationRequestDto.email());
         return null;
     }
 }
