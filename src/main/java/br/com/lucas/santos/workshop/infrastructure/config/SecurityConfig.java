143e5303a08303ff4181f5d9c87da68bc7ef030a
Lucas Santos <lucas201273@gmail.com>
2025-05-04 08:08:03 -0300

    fix: create a loader to load keys

--- src/main/java/br/com/lucas/santos/workshop/infrastructure/config/SecurityConfig.java
+++ src/main/java/br/com/lucas/santos/workshop/infrastructure/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,17 +24,22 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.nio.file.Files;
+import java.security.KeyFactory;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Value("${rsa.public-key}")
-    private RSAPublicKey publicKey;
+    private String publicKey;
     @Value("${rsa.private-key}")
-    private RSAPrivateKey privateKey;
+    private String privateKey;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -56,16 +63,39 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public RSAPublicKey publicKey() throws Exception {
+        Resource publicKeyResource = new ClassPathResource(publicKey.replace("classpath:", ""));
+        String key = new String(Files.readAllBytes(publicKeyResource.getFile().toPath()));
+        key = key.replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "").replaceAll("\\s", "");
+        byte[] decodedKey = Base64.getDecoder().decode(key);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decodedKey);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+    }
+
 
     @Bean
-    public JwtEncoder encoder(){
-        JWK jwk = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
+    public RSAPrivateKey privateKey() throws Exception {
+        Resource privateKeyResource = new ClassPathResource(privateKey.replace("classpath:", ""));
+        String key = new String(Files.readAllBytes(privateKeyResource.getFile().toPath()));
+        key = key.replace("-----BEGIN PRIVATE KEY-----", "").replace("-----END PRIVATE KEY-----", "").replaceAll("\\s", "");
+        byte[] decodedKey = Base64.getDecoder().decode(key);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decodedKey);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+    }
+
+    @Bean
+    public JwtEncoder encoder() throws Exception {
+        JWK jwk = new RSAKey.Builder(publicKey()).privateKey(privateKey()).build();
         ImmutableJWKSet<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
         return new NimbusJwtEncoder(jwks);
     }
 
+    // JwtDecoder para decodificação de JWT
     @Bean
-    public JwtDecoder decoder(){
-        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    public JwtDecoder decoder() throws Exception {
+        return NimbusJwtDecoder.withPublicKey(publicKey()).build();
     }
 }
--- src/test/java/br/com/lucas/santos/workshop/WorkshopApplicationTests.java
+++ src/test/java/br/com/lucas/santos/workshop/WorkshopApplicationTests.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = WorkshopApplication.class)
+@SpringBootTest
+@ActiveProfiles("dev")
 class WorkshopApplicationTests {
 
 	@Test
