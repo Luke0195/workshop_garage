7c555c9977737e4418e7bb6711a0346de7342342
Lucas Santos <lucas201273@gmail.com>
2025-06-11 04:41:52 -0300

    feat: ensure VehicleService should throws ResourceAlreadyExistsException when vehicle plate already exists.

--- src/test/java/br/com/lucas/santos/workshop/business/service/VehicleServiceTest.java
+++ src/test/java/br/com/lucas/santos/workshop/business/service/VehicleServiceTest.java
@@ -1,7 +1,11 @@
 package br.com.lucas.santos.workshop.business.service;
 
+import br.com.lucas.santos.workshop.domain.dto.request.VehicleRequestDto;
+import br.com.lucas.santos.workshop.factories.VehicleFactory;
+import br.com.lucas.santos.workshop.infrastructure.exceptions.ResourceAlreadyExistsException;
 import br.com.lucas.santos.workshop.infrastructure.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,12 +21,18 @@ class VehicleServiceTest {
     @InjectMocks
     private VehicleService vehicleService;
 
+    private VehicleRequestDto vehicleRequestDto;
 
-    @DisplayName("add should returns ResourceNotFoundException when vehicle name was not found")
+    @BeforeEach
+    void setup(){
+        this.vehicleRequestDto = VehicleFactory.makeVehicleRequestDto();
+    }
+
+    @DisplayName("add should returns ResourceAlreadyExists when vehicle plate already exists")
     @Test
     void addShouldReturnsResourceNotFoundExceptionWhenVehicleNameWasNotFound(){
-        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
-             vehicleService.
+        Assertions.assertThrows(ResourceAlreadyExistsException.class, () -> {
+             vehicleService.add(vehicleRequestDto);
         });
     }
 }
---
+++ src/test/java/br/com/lucas/santos/workshop/controller/VehicleControllerTest.java
@@ -0,0 +1,6 @@
+package br.com.lucas.santos.workshop.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+public class VehicleControllerTest {
+}
