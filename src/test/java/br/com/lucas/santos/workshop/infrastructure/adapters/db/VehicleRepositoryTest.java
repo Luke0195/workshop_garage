7b11957dbc4046881031521a5173c3024066b433
Lucas Santos <lucas201273@gmail.com>
2025-06-11 05:13:47 -0300

    test: ensure VehicleRepositoryTest.loadByPlateShouldReturnsAnEmptyOptionalWhenInvalidPlateIsProvided should returns an vehicle when valid plate is provided

--- src/test/java/br/com/lucas/santos/workshop/infrastructure/adapters/db/VehicleRepositoryTest.java
+++ src/test/java/br/com/lucas/santos/workshop/infrastructure/adapters/db/VehicleRepositoryTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 
 @ExtendWith(MockitoExtension.class)
